86a1ec42154d15683e245f2404b62a0c53a8d8c0
Rahul Rudra <rahulrudra665@gmail.com>
2024-02-03 09:58:42 +0600

    first commit

---
+++ Module-07/Es6Features.js
@@ -0,0 +1,24 @@
+// Template String
+
+const numbers = [12, 13];
+const student = {
+  name: "Shakib Khan",
+  age: 32
+};
+
+const about = `My name is ${student.name} age is ${numbers[0]} years old`;
+console.log(about);
+
+//Arrow function
+
+const getFiftyFive = () => {
+  return 55;
+};
+console.log(getFiftyFive());
+
+const isEven = (number) => {
+  return number % 2 == 0;
+};
+
+const number = isEven(12);
+console.log(number);
