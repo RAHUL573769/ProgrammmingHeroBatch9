05a4830fca7ad8aa0a10b37625a6f3bb286ca77b
Rahul Rudra <rahulrudra665@gmail.com>
2024-02-07 09:47:49 +0600

    module 4 started

---
+++ Milestone-08/tailwindproject/src/Components/Constans/canstantvariables.js
@@ -0,0 +1,29 @@
+export const priceOptions = [
+  {
+    name: "Product A",
+    id: "A123",
+    prices: {
+      regular: 49.99,
+      sale: 39.99,
+      discountPercentage: 20
+    }
+  },
+  {
+    name: "Product B",
+    id: "B456",
+    prices: {
+      regular: 79.99,
+      sale: 59.99,
+      discountPercentage: 25
+    }
+  },
+  {
+    name: "Product C",
+    id: "C789",
+    prices: {
+      regular: 99.99,
+      sale: 79.99,
+      discountPercentage: 20
+    }
+  }
+];
--- Milestone-08/tailwindproject/src/Components/PriceOptions/SinglePriceOptins.jsx
+++ Milestone-08/tailwindproject/src/Components/PriceOptions/SinglePriceOptins.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 // eslint-disable-next-line react/prop-types
-const SinglePriceOptins = ({ options }) => {
+const SinglePriceOptins = ({ option }) => {
+  console.log(option);
+
+  // eslint-disable-next-line react/prop-types
+  const { name, prices } = option;
   return (
-    <div>
-      <h2>Best Prices in the Town</h2>
+    <div className="bg-blue-300 rounded-md ">
+      <h2>
+        <span className="text-7xl">{prices.regular}</span>
+      </h2>
+
+      <h4>{name}</h4>
     </div>
   );
 };
