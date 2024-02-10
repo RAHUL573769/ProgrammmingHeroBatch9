23358f308c09885621efa0dd6e0eb84df36227e1
Rahul Rudra <rahulrudra665@gmail.com>
2024-02-10 08:38:51 +0600

    login started'

--- Milestone-09/Module-51/authintegration/src/Pages/Register/Register.jsx
+++ Milestone-09/Module-51/authintegration/src/Pages/Register/Register.jsx
@@ -1,5 +1,67 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div></div>;
+  const handleRegister = () => {};
+  return (
+    <div>
+      <div>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold">Register now!</h1>
+            </div>
+            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+              <form onSubmit={handleRegister} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Name</span>
+                  </label>
+                  <input
+                    type="name"
+                    name="name"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    className="input input-bordered"
+                    required
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-primary">Login</button>
+                </div>
+              </form>
+
+              <p>
+                Already Have an Account?<Link to="/Login">Login</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
